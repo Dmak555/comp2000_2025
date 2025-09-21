8532b2f22c3fe9ced64a9e48bd663d1f8fbd111c
Dmak555 <daksh.makwana@students.mq.edu.au>
2025-09-21 20:44:34 +1000

    implement drawable so actor and item share paint

--- src/Actor.java
+++ src/Actor.java
@@ -3,13 +3,15 @@ import java.awt.Graphics;
 import java.awt.Polygon;
 import java.util.List;
 
-public abstract class Actor {
+public abstract class Actor implements Drawable {
   Color color;
   Cell loc;
   List<Polygon> display;
 
-  public void paint(Graphics g) {
-    for(Polygon p: display) {
+  public void paint(Graphics g){
+    if(display ==null) 
+    return;
+    for(Polygon p: display){
       g.setColor(color);
       g.fillPolygon(p);
       g.setColor(Color.GRAY);
